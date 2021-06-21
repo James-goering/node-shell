0d048b8f2c816bd9ed1bc47fd0c338bbed79a6b2
James Goering <jamesgoering@Jamess-MacBook-Pro.local>
2021-06-21 14:55:26 -0400

    Refactored and updated ls

--- bash.js
+++ bash.js
@@ -1,6 +1,22 @@
 process.stdout.write('prompt > ');
-require('./pwd.js');
-require('./ls.js');
-// pwd();
+const pwd = require('./pwd.js');
+const ls = require('./ls.js');
+
+
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();  //remove the newline
+      if(cmd === 'pwd'){
+        pwd();
+      } else if (cmd === 'ls') {
+        ls();
+      }
+    process.stdout.write('\nYou typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+  })
+
+
+
+
+
 
 
--- pwd.js
+++ pwd.js
@@ -1,9 +1,4 @@
-let exporting = process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();  //remove the newline
-    if(cmd === 'pwd'){
-       process.stdout.write(process.cwd());
-    }
-  process.stdout.write('\nYou typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
-})
-module.exports.exporting = exporting;
+module.exports = function pwd() {
+  process.stdout.write(process.cwd());
+}
+
